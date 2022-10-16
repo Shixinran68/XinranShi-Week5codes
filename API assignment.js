a784a346eb81ef0d471f7f815e58742599bb0c02
Shixinran68 <xs2288@nyu.edu>
2022-10-15 22:55:22 -0400

    new changes

--- API assignment.js
+++ API assignment.js
@@ -1,7 +1,17 @@
-function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(225, 0, 0);
-  }
+let Billboard;
+let radio;
+
+
+function preload(){
+    Billboard = loadImage("Billboard.png");
+    radio = loadImage("radio.png")
+}
+function setup (){
+createCanvas(800, 800);
+}
+function draw(){
+    background(0);
+    image(Billboard,40, 360, 680, 400);
+    image(radio, 444, 310, 165, 130); 
+
+}
